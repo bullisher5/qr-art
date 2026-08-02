c7d723e6aee1e7288066d9608347cba4483c2e57
bullisher5 <artemaltov15@gmail.com>
2026-08-02 23:18:05 +0300

    Update generate-art-qr.js

--- netlify/functions/generate-art-qr.js
+++ netlify/functions/generate-art-qr.js
@@ -37,7 +37,7 @@ exports.handler = async (event) => {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-                version: 'zylim0702/qr_code_controlnet',
+                version: 'zylim0702/qr_code_controlnet_v2',
                 input: {
                     url: text,
                     image: image,
